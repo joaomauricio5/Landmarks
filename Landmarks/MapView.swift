716a8d7687d52c834301c6eeb978782f044c47cc
Joao Mauricio <jfmauricio5@gmail.com>
2022-02-25 16:39:47 +0000

    Added marker to map

--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -8,15 +8,31 @@
 import SwiftUI
 import MapKit
 
+
+private var turtleRockCoordinates = CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868)
+
+struct MapLocation: Identifiable {
+    let id = UUID()
+    let coordinates: CLLocationCoordinate2D
+}
+
 struct MapView: View {
     
     @State private var region = MKCoordinateRegion(
-           center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+           center: turtleRockCoordinates,
            span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
        )
     
+    var annotationItem = MapLocation(coordinates: turtleRockCoordinates)
+    
     var body: some View {
-        Map(coordinateRegion: $region)
+        
+        Map(coordinateRegion: $region,
+            interactionModes: MapInteractionModes() , /*interactionModes() is empty in order to prevent the user from moving the map */
+            annotationItems: [annotationItem])
+        { place in
+            MapMarker(coordinate: annotationItem.coordinates)
+        }
     }
 }
 
