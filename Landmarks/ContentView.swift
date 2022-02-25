46096966f992b7dffe6ea7dad0055ec7c217bb8c
Joao Mauricio <jfmauricio5@gmail.com>
2022-02-25 11:35:35 +0000

    First commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by João Mauricio UWE ID 18030724 on 25/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
