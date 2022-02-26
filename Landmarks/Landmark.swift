928a4e2cf8d175a0dc2c15cdb7ad2c8284cd9798
Joao Mauricio <jfmauricio5@gmail.com>
2022-02-26 22:14:15 +0000

    Added coordinates to Landmark struct

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
     var id: String
@@ -14,9 +15,21 @@ struct Landmark: Hashable, Codable {
     var park: String
     var state: String
     var description: String
-}
-
-private var imageName: String
-var image: Image {
-    Image(imageName)
+    
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+    }
 }
---
+++ Landmarks/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by João Mauricio UWE ID 18030724 on 26/02/2022.
+//
+
+import Foundation
