c0e52a2d091f6914929c4d313650fc2dc41248d0
Joao Mauricio <jfmauricio5@gmail.com>
2022-02-26 21:50:34 +0000

    Added JSON file with landmarks data

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,16 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by João Mauricio UWE ID 18030724 on 26/02/2022.
+//
+
+import Foundation
+
+struct Landmark {
+    var id: String
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+}
