b26980c93056ec2bcee6ae87a3d04cb3ea5e15fa
Joao Mauricio <jfmauricio5@gmail.com>
2022-03-02 15:44:25 +0000

    Completed section 4

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by João Mauricio UWE ID 18030724 on 02/03/2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
