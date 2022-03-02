8492ee16572f51183fbb205d25397edc283726ba
Joao Mauricio <jfmauricio5@gmail.com>
2022-03-02 17:37:54 +0000

    Added marker to Map View

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -9,41 +9,37 @@ import SwiftUI
 import MapKit
 
 
-//private var turtleRockCoordinates = CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868)
-
-//struct MapLocation: Identifiable {
-//    let id = UUID()
-//    let coordinates: CLLocationCoordinate2D
-//}
-
+struct MapLocation: Identifiable {
+    let id = UUID()
+    let coordinates: CLLocationCoordinate2D
+}
 
 struct MapView: View {
     
     var coordinate: CLLocationCoordinate2D
     @State private var region = MKCoordinateRegion()
+    @State private var annotationItem = MapLocation(coordinates: CLLocationCoordinate2D())
     
     
-    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+    private func setRegionAndMarker(_ coordinate: CLLocationCoordinate2D) {
         region = MKCoordinateRegion(
             center: coordinate,
             span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+        
+        annotationItem = MapLocation(coordinates: coordinate)
     }
     
-    //    var annotationItem = MapLocation(coordinates: turtleRockCoordinates)
     
     var body: some View {
         
-        //        Map(coordinateRegion: $region,
-        //            interactionModes: MapInteractionModes() , /*interactionModes() is empty in order to prevent the user from moving the map */
-        //            annotationItems: [annotationItem])
-        //        { place in
-        //            MapMarker(coordinate: annotationItem.coordinates)
-        //        }
-        
-        Map(coordinateRegion: $region)
-            .onAppear {
-                setRegion(coordinate)
-            }
+        Map(coordinateRegion: $region,
+            annotationItems: [annotationItem])
+        { place in
+            MapMarker(coordinate: annotationItem.coordinates)
+        }
+        .onAppear {
+            setRegionAndMarker(coordinate)
+        }
     }
 }
 
